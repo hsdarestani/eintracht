996d357c0ccad5727a761f9860c6ff2303f546fc
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-21 20:46:18 +0330

    Use verified privacy-safe source package

--- bootstrap-source.sh
+++ bootstrap-source.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-if [[ ! -f .bootstrap/part00 ]]; then
+if [[ ! -f .source-v2/part00 ]]; then
   echo "Application source is already expanded."
   exit 0
 fi
@@ -11,7 +11,8 @@ fi
 archive="$(mktemp --suffix=.tar.gz)"
 trap 'rm -f "$archive"' EXIT
 
-cat .bootstrap/part* | base64 --decode > "$archive"
+cat .source-v2/part* | base64 --decode > "$archive"
+echo "ace02085a8ca02a0dd39b3ffc2bcb7db58e7229017b19b51e3e9f58ec0c7b859  $archive" | sha256sum -c -
 tar -tzf "$archive" >/dev/null
 tar -xzf "$archive" -C .
 
